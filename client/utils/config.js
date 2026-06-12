9e57c5ac6df33396ff8e5153b6d7b866832f82c9
Vaibhavpandey8 <vaibhavpandey729@gmail.com>
2026-06-12 23:14:53 +0530

    Update API endpoints to production Render URL

--- client/utils/config.js
+++ client/utils/config.js
@@ -1,3 +1,3 @@
-export const API = 'http://localhost:8525/api';
-export const API_ROOT = 'http://localhost:8525';
+export const API = 'https://yatramitra-backend-9t48.onrender.com/api';
+export const API_ROOT = 'https://yatramitra-backend-9t48.onrender.com';
 export const GOOGLE_CLIENT_ID = '803300865226-1mgv9v1vlp4k5a21g2r4e5cejvkk05cp.apps.googleusercontent.com';
