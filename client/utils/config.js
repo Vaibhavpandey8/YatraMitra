acdcc5f3aa092212fcb3f5613102a2cf7a549fce
Vaibhavpandey8 <96644799+Vaibhavpandey8@users.noreply.github.com>
2026-08-02 14:27:43 +0530

    fix: update Google OAuth Client ID to match user Google Console credential

--- client/utils/config.js
+++ client/utils/config.js
@@ -1,3 +1,3 @@
 export const API = 'https://yatramitra-backend-9t48.onrender.com/api';
 export const API_ROOT = 'https://yatramitra-backend-9t48.onrender.com';
-export const GOOGLE_CLIENT_ID = '803300865226-1mgv9v1vlp4k5a21g2r4e5cejvkk05cp.apps.googleusercontent.com';
+export const GOOGLE_CLIENT_ID = '712248449982-d53mjnuovomnsgpcjkv2femg4ar83mg4.apps.googleusercontent.com';
